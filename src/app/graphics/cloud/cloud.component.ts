34edddb356275ec3ee1bd28c5c3647ffc61ea50d
maocq <mauriciocarmona@seven4n.com>
2019-10-14 14:13:01 -0500

    Ajuste componente

--- src/app/graphics/cloud/cloud.component.ts
+++ src/app/graphics/cloud/cloud.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {File} from '../../models/Group';
+import * as Highcharts from 'highcharts';
 
 @Component({
   selector: 'app-cloud',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cloud.component.sass']
 })
 export class CloudComponent implements OnInit {
+  @Input() files: File[];
+
+  public options: any = {
+    series: [{
+      type: 'wordcloud',
+      data: [],
+      name: 'Occurrences'
+    }],
+    title: {
+      text: 'Wordcloud of Lorem Ipsum'
+    }
+  };
 
   constructor() { }
 
   ngOnInit() {
+    this.options.series.data = this.files;
+    //Highcharts.chart('container-cloud', this.options);
   }
 
 }
