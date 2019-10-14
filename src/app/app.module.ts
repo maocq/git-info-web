81beb849eba814b8512a67c4b868edd1930e187b
maocq <mauriciocarmona@seven4n.com>
2019-10-14 13:45:57 -0500

    Componentes a graphics module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HttpClientModule} from '@angular/common/http';
-import { ProjectsIssuesComponent } from './graphics/projects-issues/projects-issues.component';
 import {ProjectService} from './services/project.service';
-import { BasicLineComponent } from './graphics/basic-line/basic-line.component';
-import { ColumnsComponent } from './graphics/columns/columns.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import {DataModule} from './data/data.module';
@@ -16,9 +13,6 @@ import {GraphicsModule} from './graphics/graphics.module';
 @NgModule({
   declarations: [
     AppComponent,
-    ProjectsIssuesComponent,
-    BasicLineComponent,
-    ColumnsComponent,
     HomeComponent,
     NotFoundComponent
   ],
--- src/app/graphics/graphics.module.ts
+++ src/app/graphics/graphics.module.ts
@@ -1,18 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {CloudComponent} from './cloud/cloud.component';
+import {BasicLineComponent} from './basic-line/basic-line.component';
+import {ColumnsComponent} from './columns/columns.component';
+import {ProjectsIssuesComponent} from './projects-issues/projects-issues.component';
 
 
 
 @NgModule({
   declarations: [
-    CloudComponent
+    CloudComponent,
+    BasicLineComponent,
+    ColumnsComponent,
+    ProjectsIssuesComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    CloudComponent
+    CloudComponent,
+    BasicLineComponent,
+    ColumnsComponent,
+    ProjectsIssuesComponent
   ]
 })
 export class GraphicsModule { }
