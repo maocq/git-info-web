124a7dac2f070111cafc31cfc5b02d5542940e6a
maocq <mauriciocarmona@seven4n.com>
2019-10-14 10:40:40 -0500

    Modulo innecesario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,6 @@ import {ProjectService} from './services/project.service';
 import { BasicLineComponent } from './graphics/basic-line/basic-line.component';
 import { ColumnsComponent } from './graphics/columns/columns.component';
 import { GroupsComponent } from './data/groups/groups.component';
-import { ProjectComponent } from './pages/project/project.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 
@@ -20,7 +19,6 @@ import { NotFoundComponent } from './pages/not-found/not-found.component';
     BasicLineComponent,
     ColumnsComponent,
     GroupsComponent,
-    ProjectComponent,
     HomeComponent,
     NotFoundComponent
   ],
