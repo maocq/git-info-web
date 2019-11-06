ae42863cef5dd3e06732741166c4578153f831d5
maocq <mauriciocarmona@seven4n.com>
2019-11-05 19:55:45 -0500

    Detail group

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import {NotFoundComponent} from './pages/not-found/not-found.component';
 const routes: Routes = [
   //{ path: '', component: HomeComponent },
   { path: '', redirectTo: 'groups', pathMatch: 'full' },
-  { path: 'groups', loadChildren: () => import('./group/group.module').then(m => m.GroupModule) },
+  { path: 'groups', loadChildren: () => import('./detail-group/group.module').then(m => m.GroupModule) },
   { path: 'not-found', component: NotFoundComponent },
   { path: '**', redirectTo: 'not-found', pathMatch: 'full' }
 ];
